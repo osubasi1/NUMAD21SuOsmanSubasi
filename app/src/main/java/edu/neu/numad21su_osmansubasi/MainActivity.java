22a155fbdb274849355ae5d34a4a41016b4652d6
osubasi1 <osmansubasi307@gmail.com>
2021-05-16 10:16:40 -0400

    now display name and email upon clicking button

--- app/src/main/java/edu/neu/numad21su_osmansubasi/MainActivity.java
+++ app/src/main/java/edu/neu/numad21su_osmansubasi/MainActivity.java
@@ -3,12 +3,26 @@ package edu.neu.numad21su_osmansubasi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button aboutBtn;
+    TextView infoText;
+    String email = "subasi.o@northeastern.edu";
+    String name = "Osman Subasi";
+    String info = name + "\n" + email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        aboutBtn= findViewById(R.id.about_btn);
+        infoText = findViewById(R.id.info_text);
+        aboutBtn.setOnClickListener(v -> infoText.setText(info));
+
     }
+
+
 }
