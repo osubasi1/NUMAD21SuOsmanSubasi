718c5d3b9842b382fb86fe6e1096b673c105f68b
osubasi1 <osmansubasi307@gmail.com>
2021-06-13 22:45:44 -0400

    final version

--- app/src/main/java/edu/neu/numad21su_osmansubasi/LocationActivity.java
+++ app/src/main/java/edu/neu/numad21su_osmansubasi/LocationActivity.java
@@ -24,8 +24,10 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
     boolean isGPSEnabled = false;
     boolean isNetworkEnabled = false;
     Location location;
-    double dLatitude; // latitude
-    double dLongitude; // longitude
+    Double dLatitude; // latitude
+    Double dLongitude; // longitude
+    long minTime = 1000 * 60; // min time to request update (1 minute)
+    long minDistance = 10; // min distance (10 meters)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +46,17 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
                 ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
             }
+            getNetworkLocation();
             getGpsLocation();
-            System.out.println("is location null"+ location.getLongitude());
-            System.out.println("dLatitude is:" + dLatitude);
-            System.out.println("dLongitude is:" + dLongitude);
 
+            // Interestingly in a physical device GPS does not work but network works
+            // and in emulator gps works but network does not work so this is the solution that
+            // I have found.
+            if (dLatitude == null || dLongitude == null){
+                getNetworkLocation();
+            } else {
+                getGpsLocation();
+            }
         } catch (Exception error){
             error.printStackTrace();
         }
@@ -60,11 +68,16 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
             if (isGPSEnabled){
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions((Activity) getApplicationContext(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions((Activity) getApplicationContext(),
+                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
                 }
                 locationManager.requestLocationUpdates(
-                        LocationManager.GPS_PROVIDER,1000*60/30, 10, this);
+                        LocationManager.GPS_PROVIDER, minTime, minDistance, this);
+
             }
             Log.d("GPS Enabled", "GPS Enabled");
             if(locationManager != null){
@@ -78,17 +91,33 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         catch (Exception error){
             error.printStackTrace();
         }
-
-
     }
     private void getNetworkLocation(){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if (isNetworkEnabled) {
+            //check the network permission
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions((Activity) getApplicationContext(),
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+            }
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, minTime, minDistance, this);
 
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            Log.d("Network", "Network");
+            if (locationManager != null) {
+                location = locationManager
+                        .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
+                if (location != null) {
+                    dLatitude = location.getLatitude();
+                    dLongitude = location.getLongitude();
+                }
+            }
+        }
     }
 
     @Override
